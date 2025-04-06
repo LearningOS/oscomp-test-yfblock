5868edea6166067eca8dc50a3a3f32a44f7f49ee
yfblock <321353225@qq.com>
2025-04-06 17:47:25 +0800

    update test

--- tools/testcase-riscv64/libctest_testcode.sh
+++ tools/testcase-riscv64/libctest_testcode.sh
@@ -1,4 +1,6 @@
-echo START libctest-musl
+echo '#### OS COMP TEST GROUP START libctest-glibc ####'
+echo '#### OS COMP TEST GROUP START libctest-musl ####'
 ./run-static.sh
 ./run-dynamic.sh
-echo END libctest-musl
+echo '#### OS COMP TEST GROUP END libctest-musl ####'
+echo '#### OS COMP TEST GROUP END libctest-glibc ####'
